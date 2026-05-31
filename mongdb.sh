f7328a17eead4baa3835101f486bf9401cc56a1d
AnilKumar M <anilkumarmidde05@gmail.com>
2026-05-31 14:47:31 +0530

    mongodbcommonsource

---
+++ mongdb.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+source ./common.sh
+
+check_root
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+VALIDATE $? "Copying mongodb repo"
+
+dnf install mongodb-org -y &>>$LOGS_FILE
+VALIDATE $? "Installing mongodb server"
+
+systemctl enable mongod &>>$LOGS_FILE
+VALIDATE $? "Enable mongodb"
+
+systemctl start mongod
+VALIDATE $? "Start mongodb"
+
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
+VALIDATE $? "allowing remote connections"
+
+systemctl restart mongod
+VALIDATE $? "Restarted mongoDB"
+
+
+print_total_time()
